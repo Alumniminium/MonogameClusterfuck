67ccb011f04d370dda740db7d3b9b35b0632478c
Dom <domi@outlook.at>
2019-02-12 13:40:23 +0100

    Added *working* selection rectangle

--- Systems/Cursor.cs
+++ Systems/Cursor.cs
@@ -6,28 +6,87 @@ using MonoGameClusterFuck.Primitives;
 
 namespace MonoGameClusterFuck.Systems
 {
-    public class Cursor : Sprite
+    public class SelectionRectangle : Sprite
     {
-        Vector2 CursorVector;
         Vector2 SelectionPosition;
         Vector2 SelectionEndPosition;
-        Rectangle[] SelectionRectangle;
-        Texture2D SelectionTexture;
+        Rectangle[] Lines;
         Color BorderColor;
         int BorderThickness = 2;
-        public Cursor(int size) : base(size)
+        public SelectionRectangle(int size) : base(size)
         {
-            CursorVector = Vector2.Zero;
             SelectionPosition = Vector2.Zero;
-            SelectionRectangle = new Rectangle[4];
+            Lines = new Rectangle[4];
             BorderColor = Color.White;
         }
 
         public override void LoadContent()
         {
+            Texture = new Texture2D(Game.Instance.GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
+            Texture.SetData(new[] { Color.White });
+        }
+        public override void Update(GameTime deltaTime)
+        {
+            var X = Game.Instance.InputManager.MouseState.X;
+            var Y = Game.Instance.InputManager.MouseState.Y;
+            if (Game.Instance.InputManager.MouseState.LeftButton == ButtonState.Pressed && Game.Instance.InputManager.LastMouseState.LeftButton != ButtonState.Pressed)
+            {
+                SelectionPosition.X = X;
+                SelectionPosition.Y = Y;
+            }
+            if (Game.Instance.InputManager.MouseState.LeftButton == ButtonState.Pressed && Game.Instance.InputManager.LastMouseState.LeftButton == ButtonState.Pressed)
+            {
+                var x = Math.Min(SelectionPosition.X, X);
+                var y = Math.Min(SelectionPosition.Y, Y);
+                SelectionEndPosition.X = (Math.Abs(SelectionPosition.X - X) / 32) * 32;
+                SelectionEndPosition.Y = (Math.Abs(SelectionPosition.Y - Y) / 32) * 32;
+
+                Lines[0].X = (int)x;
+                Lines[0].Y = (int)y;
+                Lines[0].Width = (int)SelectionEndPosition.X;
+                Lines[0].Height = BorderThickness;
+
+                Lines[1].X = (int)x;
+                Lines[1].Y = (int)y;
+                Lines[1].Width = BorderThickness;
+                Lines[1].Height = (int)SelectionEndPosition.Y;
+
+                Lines[2].X = (int)x + (int)SelectionEndPosition.X - BorderThickness;
+                Lines[2].Y = (int)y;
+                Lines[2].Width = BorderThickness;
+                Lines[2].Height = (int)SelectionEndPosition.Y;
+
+                Lines[3].X = (int)x;
+                Lines[3].Y = (int)y + (int)SelectionEndPosition.Y - BorderThickness;
+                Lines[3].Width = (int)SelectionEndPosition.X;
+                Lines[3].Height = BorderThickness;
+            }
+            if (Game.Instance.InputManager.MouseState.LeftButton == ButtonState.Released)
+            {
+                for(int i=0;i<Lines.Length;i++)
+                    Lines[i]= Rectangle.Empty;
+            }
+        }
+        public override void Draw()
+        {
+            for(int i =0;i<Lines.Length;i++)
+                Game.Instance.SpriteBatch.Draw(Texture,Lines[i],Source,Color.White);
+        }
+    }
+    public class Cursor : Sprite
+    {
+        SelectionRectangle SelectionRect;
+        Vector2 CursorVector;
+        public Cursor(int size) : base(size)
+        {
+            CursorVector = Vector2.Zero;
+            SelectionRect=new SelectionRectangle(1);
+        }
+
+        public override void LoadContent()
+        {
             Texture = Game.Instance.Content.Load<Texture2D>("selectionrect");
-            SelectionTexture = new Texture2D(Game.Instance.GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
-            SelectionTexture.SetData(new[] { Color.White });
+            SelectionRect.LoadContent();
         }
         public override void Update(GameTime deltaTime)
         {
@@ -37,50 +96,12 @@ namespace MonoGameClusterFuck.Systems
             Position.X = (int)(CursorVector.X / 32) * 32 + (int)RotationOrigin.X;
             Position.Y = (int)(CursorVector.Y / 32) * 32 + (int)RotationOrigin.Y;
             Destination.Location = Position;
-            //Not sure what to do here 
-            if (Game.Instance.InputManager.MouseState.LeftButton == ButtonState.Pressed && Game.Instance.InputManager.LastMouseState.LeftButton != ButtonState.Pressed)
-            {
-                SelectionPosition.X = Position.X;
-                SelectionPosition.Y = Position.Y;
-            }
-            if (Game.Instance.InputManager.MouseState.LeftButton == ButtonState.Pressed && Game.Instance.InputManager.LastMouseState.LeftButton == ButtonState.Pressed)
-            {
-                var x = Math.Min(SelectionPosition.X, CursorVector.X);
-                var y = Math.Min(SelectionPosition.Y, CursorVector.Y);
-                SelectionEndPosition.X = (Math.Abs(SelectionPosition.X - CursorVector.X) / 32) * 32;
-                SelectionEndPosition.Y = (Math.Abs(SelectionPosition.Y - CursorVector.Y) / 32) * 32;
-
-                SelectionRectangle[0].X = (int)x;
-                SelectionRectangle[0].Y = (int)y;
-                SelectionRectangle[0].Width = (int)SelectionEndPosition.X;
-                SelectionRectangle[0].Height = BorderThickness;
-
-                SelectionRectangle[1].X = (int)x;
-                SelectionRectangle[1].Y = (int)y;
-                SelectionRectangle[1].Width = BorderThickness;
-                SelectionRectangle[1].Height = (int)SelectionEndPosition.Y;
-
-                SelectionRectangle[2].X = (int)x + (int)SelectionEndPosition.X - BorderThickness;
-                SelectionRectangle[2].Y = (int)y;
-                SelectionRectangle[2].Width = BorderThickness;
-                SelectionRectangle[2].Height = (int)SelectionEndPosition.Y;
-
-                SelectionRectangle[3].X = (int)x;
-                SelectionRectangle[3].Y = (int)y + (int)SelectionEndPosition.Y - BorderThickness;
-                SelectionRectangle[3].Width = (int)SelectionEndPosition.X;
-                SelectionRectangle[3].Height = BorderThickness;
-            }
-            if (Game.Instance.InputManager.MouseState.LeftButton == ButtonState.Released)
-            {
-                for(int i=0;i<SelectionRectangle.Length;i++)
-                    SelectionRectangle[i]= Rectangle.Empty;
-            }
+            SelectionRect.Update(deltaTime);
         }
         public override void Draw()
         {
-            for (int i = 0; i < SelectionRectangle.Length; i++)
-                Game.Instance.SpriteBatch.Draw(SelectionTexture, SelectionRectangle[i], BorderColor);
             base.Draw();
+            SelectionRect.Draw();
         }
     }
 }
